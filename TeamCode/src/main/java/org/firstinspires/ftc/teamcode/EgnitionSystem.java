9e102dda2707cb032ccc5db985f29a83939d4dd6
Hava <avivaviv11223344@gmail.com>
2024-09-04 14:50:19 +0300

    created a new project for new season

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EgnitionSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EgnitionSystem.java
@@ -7,73 +7,72 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.IMU;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 public class EgnitionSystem {
     // Variables
-    static private DcMotor fl_wheel;
-    static private DcMotor fr_wheel;
-    static private DcMotor bl_wheel;
-    static private DcMotor br_wheel;
+    static private DcMotor [] Motors;
     static private IMU imu;
     static private double maxPower;
     static private double lx;
     static private double ly;
     static private double rx;
-    static private double heading;
+    static private double imuHeadingRadians;
     static private double adjustedLx;
     static private double adjustedLy;
 
-    // Initializing
+    // Initializing function
     public static void init(DcMotor fl_wheel, DcMotor fr_wheel, DcMotor bl_wheel, DcMotor br_wheel, IMU imu) {
-        EgnitionSystem.fl_wheel = fl_wheel;
-        EgnitionSystem.fr_wheel = fr_wheel;
-        EgnitionSystem.bl_wheel = bl_wheel;
-        EgnitionSystem.br_wheel = br_wheel;
-
-        EgnitionSystem.bl_wheel.setDirection(DcMotorSimple.Direction.REVERSE);
-        EgnitionSystem.fl_wheel.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        fl_wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        fr_wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        bl_wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        br_wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
+        // Assigning objects to variables
+        EgnitionSystem.Motors[0] = fl_wheel;
+        EgnitionSystem.Motors[1] = fr_wheel;
+        EgnitionSystem.Motors[2] = bl_wheel;
+        EgnitionSystem.Motors[3] = br_wheel;
         EgnitionSystem.imu = imu;
 
+        // Setting motors behavior
+        EgnitionSystem.Motors[0].setDirection(DcMotorSimple.Direction.REVERSE);
+        EgnitionSystem.Motors[2].setDirection(DcMotorSimple.Direction.REVERSE);
+
+        for (DcMotor motor : Motors) {
+            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+
+        // Setting imu behavior
         EgnitionSystem.imu.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.UP,
                 RevHubOrientationOnRobot.UsbFacingDirection.FORWARD)));
         EgnitionSystem.imu.resetYaw();
 
-        //Setting variables
+        // Setting variables
         maxPower = 1;
         lx = 0;
         ly = 0;
         rx = 0;
-        heading = 0;
+        imuHeadingRadians = 0;
         adjustedLx = 0;
         adjustedLy = 0;
     }
 
+    // Main drive function
     public static void driveTeleOp(Gamepad gamepad1) {
-        //Setting variables
+        // Setting variables
         lx = gamepad1.left_stick_x;
         ly = -gamepad1.left_stick_y;
         rx = gamepad1.right_stick_x;
+        imuHeadingRadians = -imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
+        // Calculating maxPower for saving the ratio of motor powers
         maxPower = Math.max(Math.abs(lx) + Math.abs(ly) + Math.abs(rx), 1);
 
-        heading = -imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+        // Calculating adjusted powers for field centric drive
+        adjustedLx = -ly * Math.sin(imuHeadingRadians) + lx * Math.cos(imuHeadingRadians);
+        adjustedLy = ly * Math.cos(imuHeadingRadians) + lx * Math.sin(imuHeadingRadians);
 
-        adjustedLx = -ly * Math.sin(heading) + lx * Math.cos(heading);
-        adjustedLy = ly * Math.cos(heading) + lx * Math.sin(heading);
-
-        //Giving power to motors
-        fl_wheel.setPower((adjustedLy + adjustedLx + rx) / maxPower);
-        bl_wheel.setPower((adjustedLy - adjustedLx + rx) / maxPower);
-        fr_wheel.setPower((adjustedLy - adjustedLx - rx) / maxPower);
-        br_wheel.setPower((adjustedLy + adjustedLx - rx) / maxPower);
+        // Giving power to motors
+        Motors[0].setPower((adjustedLy + adjustedLx + rx) / maxPower);
+        Motors[1].setPower((adjustedLy - adjustedLx - rx) / maxPower);
+        Motors[2].setPower((adjustedLy - adjustedLx + rx) / maxPower);
+        Motors[3].setPower((adjustedLy + adjustedLx - rx) / maxPower);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PurePursuitAlpha.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PurePursuitAlpha.java
@@ -1,4 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.geometry.Pose2d;
+import com.arcrobotics.ftclib.purepursuit.Path;
+import com.arcrobotics.ftclib.purepursuit.Waypoint;
+
 public class PurePursuitAlpha {
+
 }
