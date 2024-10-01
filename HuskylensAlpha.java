dbee5e0d6d68170ef445b0f0e408fc7e861170c5
BarnYard#22993 <barnyard22993@gmail.com>
2024-10-01 15:20:16 +0300

    Add files via upload

---
+++ HuskylensAlpha.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.dfrobot.HuskyLens;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.internal.system.Deadline;
+
+import java.util.concurrent.TimeUnit;
+
+public class HuskylensAlpha{
+
+}
