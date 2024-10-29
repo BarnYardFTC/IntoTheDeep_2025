be4c06e0f6430343a58d36b682daed467cfb5f4c
BarnYard <barnyard22993@gmail.com>
2024-10-29 20:06:00 +0200

    Tested servo props new movement function. (Not tested).

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/ServoPropsTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/ServoPropsTest.java
@@ -7,23 +7,48 @@ import org.junit.jupiter.api.Test;
 
 class ServoPropsTest {
 
+    private static final ServoProps servoProps = new ServoProps();
+
     @Test
-    void servoPosTo1() {
-        ServoProps servoProps = new ServoProps();
+    void servoPosTo1Default() {
         assertEquals(1, servoProps.getServoTargetPosition(355));
     }
 
     @Test
-    void servoPosTo0() {
-        ServoProps servoProps = new ServoProps();
+    void servoPosTo0Default() {
         assertEquals(0, servoProps.getServoTargetPosition(0));
     }
 
     @Test
-    void servoPosToMinus1() {
-        ServoProps servoProps = new ServoProps();
-        assertThrows(IllegalArgumentException.class, () -> {
-            servoProps.getServoTargetPosition(-355);
-        });
+    void servoPosToHalfDefault() {
+        assertEquals((double) 180 / 355, servoProps.getServoTargetPosition(180));
+    }
+
+    @Test
+    void servoPosToMinus1Error() {
+        assertThrows(IllegalArgumentException.class, () -> servoProps.getServoTargetPosition(-355));
+    }
+
+    @Test
+    void servoPosTo2Error() {
+        assertThrows(IllegalArgumentException.class, () -> servoProps.getServoTargetPosition(710));
+    }
+
+    @Test
+    void servoPosTo1() {
+        ServoProps servoProps = new ServoProps(355, 0, 0.5);
+        assertEquals(1, servoProps.getServoTargetPosition(177.5));
+    }
+
+    @Test
+    void servoPost1Quarter() {
+        ServoProps servoProps = new ServoProps(355, 0, 0.25);
+        assertEquals(1, servoProps.getServoTargetPosition(88.75));
+    }
+
+    @Test
+    void servoPosToHalf() {
+        ServoProps servoProps = new ServoProps(355, 0, 0.5);
+        assertEquals(0.5, servoProps.getServoTargetPosition(88.75));
     }
 }
