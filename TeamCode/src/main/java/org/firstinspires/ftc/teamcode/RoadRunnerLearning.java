1a0e570f8526d14bbba92912a090b0eba64d868a
BarnYard <barnyard22993@gmail.com>
2024-10-31 17:43:13 +0200

    Added road runner learning class. (not tested).

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerLearning.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+// Import
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.RR.MecanumDrive;
+
+@Config
+@Autonomous(name = "ROAD_RUNNER_LEARNING", group = "Autonomous")
+public class RoadRunnerLearning extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        Pose2d initialPose = new Pose2d(11.8, 61.7, Math.toRadians(90));
+        MecanumDrive ignitionSystem = new MecanumDrive(hardwareMap, initialPose);
+
+        TrajectoryActionBuilder tab = ignitionSystem.actionBuilder(initialPose)
+                .lineToYSplineHeading(33, Math.toRadians(0))
+                .waitSeconds(2)
+                .setTangent(Math.toRadians(90))
+                .lineToY(48)
+                .setTangent(Math.toRadians(0))
+                .lineToX(32)
+                .strafeTo(new Vector2d(44.5, 30))
+                .turn(Math.toRadians(180))
+                .lineToX(47.5)
+                .waitSeconds(3);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        tab.build()
+                )
+        );
+    }
+}
