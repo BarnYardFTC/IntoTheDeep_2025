43d848241a59e57fc014b53e3c7fee4c3e495189
Igal Shwartzman <yigalsh912@gmail.com>
2024-11-02 20:53:41 +0200

    Formatted. (Not tested).

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RoadRunnerLearning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RoadRunnerLearning.java
@@ -3,8 +3,6 @@ package org.firstinspires.ftc.teamcode.autonomous;
 // Import
 
 import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
@@ -24,26 +22,12 @@ public class RoadRunnerLearning extends LinearOpMode {
         Pose2d initialPose = new Pose2d(11.8, 61.7, Math.toRadians(90));
         MecanumDrive ignitionSystem = new MecanumDrive(hardwareMap, initialPose);
 
-        TrajectoryActionBuilder tab = ignitionSystem.actionBuilder(initialPose)
-                .lineToYSplineHeading(33, Math.toRadians(0))
-                .waitSeconds(2)
-                .setTangent(Math.toRadians(90))
-                .lineToY(48)
-                .setTangent(Math.toRadians(0))
-                .lineToX(32)
-                .strafeTo(new Vector2d(44.5, 30))
-                .turn(Math.toRadians(180))
-                .lineToX(47.5)
-                .waitSeconds(3);
+        TrajectoryActionBuilder tab = ignitionSystem.actionBuilder(initialPose).lineToYSplineHeading(33, Math.toRadians(0)).waitSeconds(2).setTangent(Math.toRadians(90)).lineToY(48).setTangent(Math.toRadians(0)).lineToX(32).strafeTo(new Vector2d(44.5, 30)).turn(Math.toRadians(180)).lineToX(47.5).waitSeconds(3);
 
         waitForStart();
 
         if (isStopRequested()) return;
 
-        Actions.runBlocking(
-                new SequentialAction(
-                        tab.build()
-                )
-        );
+        Actions.runBlocking(new SequentialAction(tab.build()));
     }
 }
