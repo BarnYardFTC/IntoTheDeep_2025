28c92dfd9144ac5c05673f94c32b74f436d56126
Igal Shwartzman <yigalsh912@gmail.com>
2024-11-08 11:21:21 +0200

    Made a program for red on the sample side.

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -15,12 +15,13 @@ public class MeepMeepTesting {
         Pose2d initialPose = new Pose2d(-24, -58, Math.toRadians(90));
 
         botTest.runAction(botTest.getDrive().actionBuilder(initialPose)
-                .setTangent(0)
-                .splineToConstantHeading(new Vector2d(-10, -35.5), Math.toRadians(90))
-                .strafeToLinearHeading(new Vector2d(-48, -40), Math.toRadians(45))
-                .strafeTo(new Vector2d(-55, -55))
-                .setTangent(0)
-                .splineToLinearHeading(new Pose2d(-24, -58, Math.toRadians(90)), 0)
+                .strafeToLinearHeading(new Vector2d(-10, -35.5), Math.toRadians(90))
+                .strafeTo(new Vector2d(-48, -40))
+                .strafeToLinearHeading(new Vector2d(-56.5, -56.5), Math.toRadians(45))
+                .strafeToLinearHeading(new Vector2d(-58, -40), Math.toRadians(90))
+                .strafeToLinearHeading(new Vector2d(-56.5, -56.5), Math.toRadians(45))
+                .strafeToLinearHeading(new Vector2d(-58, -40), Math.toRadians(90))
+                .strafeToLinearHeading(new Vector2d(-25, -16), Math.toRadians(0))
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
