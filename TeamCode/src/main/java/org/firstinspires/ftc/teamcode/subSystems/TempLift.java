912342955d4267b28fd8a92dd311c1a21817d9a6
Hava <avivaviv11223344@gmail.com>
2024-11-13 17:29:10 +0200

    Started coding the lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/TempLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/TempLift.java
@@ -2,6 +2,24 @@ package org.firstinspires.ftc.teamcode.subSystems;
 
 // Imports.
 
-public class TempLift {
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+public class TempLift {
+    private static final DcMotorEx[] motors = new DcMotorEx[2];
+    private static final int RIGHT = 0;
+    private static final int LEFT = 1;
+
+    public static void init(DcMotorEx right, DcMotorEx left) {
+        motors[LEFT] = left;
+        motors[RIGHT] = right;
+
+        motors[LEFT].setDirection(DcMotorSimple.Direction.REVERSE);
+
+        for (DcMotorEx motor: motors) {
+            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        }
+    }
 }
