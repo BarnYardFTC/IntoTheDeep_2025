98ec86deff7cde0d8056b994aadf077c7730c5c6
admin <barnyard22993@gmail.com>
2024-11-21 13:29:20 +0200

    Added lift props.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/LiftProps.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.modules;
+
+public class LiftProps extends MotorProps{
+    private final double PITCH_LENGTH;
+    private final int SLIDE_COUNT;
+    private final double ENCODER_TO_CM;
+
+    public LiftProps() {
+        super();
+        PITCH_LENGTH = 1188;
+        SLIDE_COUNT = 1;
+        ENCODER_TO_CM = this.ENCODER_RESOLUTION * this.OUTER_GEAR_RATIO * PITCH_LENGTH * SLIDE_COUNT / 10;
+    }
+
+    public LiftProps(double PITCH_LENGTH, int SLIDE_COUNT, double ENCODER_RESOLUTION, double OUTER_GEAR_RATIO) {
+        super(ENCODER_RESOLUTION, OUTER_GEAR_RATIO);
+        this.PITCH_LENGTH = PITCH_LENGTH;
+        this.SLIDE_COUNT = SLIDE_COUNT;
+        ENCODER_TO_CM = this.ENCODER_RESOLUTION * this.OUTER_GEAR_RATIO * PITCH_LENGTH * SLIDE_COUNT / 10;
+    }
+
+    public double getEncodersToCm(double cm) {
+        return ENCODER_TO_CM * cm;
+    }
+}
