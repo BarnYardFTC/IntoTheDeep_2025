403c7c0b667702f6aa5e324facbcc874c0ecf920
admin <barnyard22993@gmail.com>
2024-11-21 13:29:59 +0200

    Added get to everything.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/MotorProps.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/MotorProps.java
@@ -3,30 +3,42 @@ package org.firstinspires.ftc.teamcode.modules;
 // Imports.
 
 public class MotorProps {
-    private final double encoderResolution; // Amount of encoders in one motor shaft rotation..
-    private final double outerGearRatio; // Ratio of outer motor shaft rotation to one final gear rotation.
-    private final double encoderToDegree; // Encoders in one degree of movement.
+    protected final double ENCODER_RESOLUTION; // Amount of encoders in one motor shaft rotation..
+    protected final double OUTER_GEAR_RATIO; // Number of motor shaft rotations to one final gear rotation.
+    protected final double ENCODER_TO_DEGREE; // Encoders in one degree of movement.
 
     /**
      * Default constructor for standard motor.
      * A 19.2 motor, 5203.
      */
     public MotorProps() {
-        this.encoderResolution = 537.7;
-        this.outerGearRatio = 1;
-        this.encoderToDegree = encoderResolution / 360;
+        this.ENCODER_RESOLUTION = 537.7;
+        this.OUTER_GEAR_RATIO = 1;
+        this.ENCODER_TO_DEGREE = ENCODER_RESOLUTION / 360;
     }
 
     /**
      * Non standard constructor.
      *
-     * @param encoderResolution - Encoders needed for one shaft rotation.
-     * @param outerGearRatio    - Gear ratio of outer motor shaft rotation to one final gear rotation.
+     * @param ENCODER_RESOLUTION - Encoders needed for one shaft rotation.
+     * @param OUTER_GEAR_RATIO    - Gear ratio of outer motor shaft rotation to one final gear rotation.
      */
-    public MotorProps(double encoderResolution, double outerGearRatio) {
-        this.encoderResolution = encoderResolution;
-        this.outerGearRatio = outerGearRatio;
-        this.encoderToDegree = encoderResolution / 360 / outerGearRatio;
+    public MotorProps(double ENCODER_RESOLUTION, double OUTER_GEAR_RATIO) {
+        this.ENCODER_RESOLUTION = ENCODER_RESOLUTION;
+        this.OUTER_GEAR_RATIO = OUTER_GEAR_RATIO;
+        this.ENCODER_TO_DEGREE = ENCODER_RESOLUTION / 360 * OUTER_GEAR_RATIO;
+    }
+
+    public double getENCODER_RESOLUTION() {
+        return ENCODER_RESOLUTION;
+    }
+
+    public double getOUTER_GEAR_RATIO() {
+        return OUTER_GEAR_RATIO;
+    }
+
+    public double getENCODER_TO_DEGREE() {
+        return ENCODER_TO_DEGREE;
     }
 
     /**
@@ -36,6 +48,6 @@ public class MotorProps {
      * @return - Number encoders needed for moving the amount of degrees given.
      */
     public int getAngleToEncoder(double angle) {
-        return (int) (angle * encoderToDegree);
+        return (int) (angle * ENCODER_TO_DEGREE);
     }
 }
