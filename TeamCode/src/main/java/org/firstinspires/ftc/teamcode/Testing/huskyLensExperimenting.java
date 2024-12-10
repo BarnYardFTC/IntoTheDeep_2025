4aac1e13e97ce6ba589e47f20a97e1b76cceba35
admin <barnyard22993@gmail.com>
2024-12-10 13:19:08 +0200

    Changed algorithm for test.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/huskyLensExperimenting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/huskyLensExperimenting.java
@@ -13,6 +13,7 @@ public class huskyLensExperimenting extends LinearOpMode {
 
     private final int READ_PERIOD = 1;
     private HuskyLens huskyLens;
+
     @Override
     public void runOpMode() throws InterruptedException {
         huskyLens = hardwareMap.get(HuskyLens.class, "huskyLens");
@@ -30,7 +31,7 @@ public class huskyLensExperimenting extends LinearOpMode {
         double w0 = 7, h0 = 3;
         double w1 = 7, h1 = 3;
         double degree = 0;
-        while(opModeIsActive()){
+        while (opModeIsActive()) {
             if (!rateLimit.hasExpired()) {
                 continue;
             }
@@ -45,16 +46,13 @@ public class huskyLensExperimenting extends LinearOpMode {
 //                }
                 w1 = blocks[i].width;
                 h1 = blocks[i].height;
-                if (w0 == h1 && h0 == w1){
+                if (w0 == h1 && h0 == w1) {
                     degree = 90;
-                }
-                else if (w0 == w1 && h0 == h1){
+                } else if (w0 == w1 && h0 == h1) {
                     degree = 0;
-                }
-                else {
-                    degree = Math.toDegrees(Math.atan(
-                            Math.abs( (w0-h0*w1/h1) / (w0*w1/h1-h0) )
-                    ));
+                } else {
+//                    degree = Math.toDegrees(Math.atan(Math.abs((w0 - h0 * w1 / h1) / (w0 * w1 / h1 - h0))));
+                    degree = Math.toDegrees(Math.atan((w0 - h0 * w1 / h1) / (w0 * w1 / h1 - h0)));
                 }
             }
             telemetry.addData("w0", w0);
