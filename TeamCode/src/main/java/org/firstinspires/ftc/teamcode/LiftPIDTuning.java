59f6f190c147ecc387bb695e02aea08089ebdb11
admin <barnyard22993@gmail.com>
2024-12-25 23:40:36 +0200

    Classes for pid tuning.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftPIDTuning.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+// Imports.
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subSystems.Lift;
+import org.firstinspires.ftc.teamcode.subSystems.LiftArm;
+
+// TeleOp name.
+@TeleOp(name = "Lift_PID")
+
+public class LiftPIDTuning extends LinearOpMode {
+    private final FtcDashboard dash = FtcDashboard.getInstance();
+
+    @Override
+    public void runOpMode() {
+        Lift lift = new Lift(this);
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        waitForStart();
+
+        // Main Loop
+        while (opModeIsActive()) {
+            Lift.liftPID();
+
+            telemetry.addData("pos", Lift.getTargetPos());
+            telemetry.addData("current", Lift.getRightMotor().getCurrentPosition());
+            telemetry.update();
+        }
+    }
+}
