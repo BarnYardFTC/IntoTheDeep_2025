96316045f4e6ffcafb776d74f29d598e6eca5456
Aviv Dukhovich <avivaviv11223344@gmail.com>
2025-01-18 15:20:55 +0200

    Worked a tiny bit on TeleOp (Aviv)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpFunctions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpFunctions.java
@@ -11,17 +11,29 @@ import org.firstinspires.ftc.teamcode.subSystems.Lift;
 import org.firstinspires.ftc.teamcode.subSystems.LiftArm;
 
 public class TeleOpFunctions {
-    private static boolean reseted;
+
+    /*
+    The only variable the class requires as an input.
+    The other variable are variables which are initialized based on this input variable.
+    */
+    private static Gamepad gamepad;
+
+    //
+    private static boolean is_reset;
     private static GamepadEx gamepadEx;
     private static TriggerReader RIGHT_TRIGGER;
     private static TriggerReader LEFT_TRIGGER;
 
-    public static boolean isReseted() {
-        return reseted;
+    public static void setGamepad(Gamepad gamepad){
+
     }
 
-    public static void setReseted(boolean reseted) {
-        TeleOpFunctions.reseted = reseted;
+    public static boolean isReset() {
+        return is_reset;
+    }
+
+    public static void setIs_reset(boolean is_reset) {
+        TeleOpFunctions.is_reset = is_reset;
     }
 
     public static GamepadEx getGamepadEx() {
@@ -36,8 +48,8 @@ public class TeleOpFunctions {
         return RIGHT_TRIGGER;
     }
 
-    public static void setRightTrigger(TriggerReader rightTrigger) {
-        RIGHT_TRIGGER = rightTrigger;
+    public static void initializeRightTrigger() {
+        RIGHT_TRIGGER = new TriggerReader(gamepadEx, GamepadKeys.Trigger.RIGHT_TRIGGER, 0.1);
     }
 
     public static TriggerReader getLeftTrigger() {
@@ -49,9 +61,9 @@ public class TeleOpFunctions {
     }
 
     private static void resetRobot() {
-        if (reseted) {
+        if (is_reset) {
             reset();
-            reseted = true;
+            is_reset = true;
         }
     }
 
