5f6714dc28273f73dda8b90650b93780b7053a32
Hava <avivaviv11223344@gmail.com>
2025-01-22 16:37:19 +0200

    auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Programs/RedSpecimen/RedSpecimen5Park.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Programs/RedSpecimen/RedSpecimen5Park.java
@@ -9,7 +9,10 @@ import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.autonomous.Coordinates.BlueSpecimenCoordinates;
+import org.firstinspires.ftc.teamcode.autonomous.Coordinates.RedSpecimenCoordinates;
 import org.firstinspires.ftc.teamcode.autonomous.Trajactories.RedSpecimenTrajectories;
+import org.firstinspires.ftc.teamcode.roadRunner.MecanumDrive;
 
 @Config
 @Autonomous(name = "Red_Specimen_5_Park", group = "Autonomous")
@@ -17,18 +20,57 @@ import org.firstinspires.ftc.teamcode.autonomous.Trajactories.RedSpecimenTraject
 public class RedSpecimen5Park extends LinearOpMode {
     @Override
     public void runOpMode() {
-        waitForStart();
+        MecanumDrive ignitionSystem = new MecanumDrive(hardwareMap, RedSpecimenCoordinates.getStart());
 
-        Action scorePreLoad = RedSpecimenTrajectories.scorePreload.build();
-        Action moveSpecimens = RedSpecimenTrajectories.moveSpecimens.build();
-        Action scoreSecond = RedSpecimenTrajectories.scoreSecond.build();
-        Action scoreThird = RedSpecimenTrajectories.scoreThird.build();
-        Action scoreFourth = RedSpecimenTrajectories.scoreFourth.build();
-        Action scoreFifth = RedSpecimenTrajectories.scoreFifth.build();
-        Action intakeThird = RedSpecimenTrajectories.intakeThird.build();
-        Action intakeFourth = RedSpecimenTrajectories.intakeFourth.build();
-        Action intakeFifth = RedSpecimenTrajectories.intakeFifth.build();
-        Action park = RedSpecimenTrajectories.park.build();
+        Action scorePreLoad = ignitionSystem.actionBuilder(RedSpecimenCoordinates.getStart())
+                .strafeToConstantHeading(RedSpecimenCoordinates.getScore1().position).build();
+
+        Action moveSpecimens = ignitionSystem.actionBuilder(RedSpecimenCoordinates.getScore1())
+                .setTangent(RedSpecimenCoordinates.getMidWayMoveSpecimensTangent())
+                .splineToConstantHeading(RedSpecimenCoordinates.getMidWayMoveSpecimens().position, RedSpecimenCoordinates.getStart().heading)
+
+                .splineToConstantHeading(RedSpecimenCoordinates.getMoveSpecimensStart0().position, RedSpecimenCoordinates.getStart().heading)
+
+                .splineToConstantHeading(RedSpecimenCoordinates.getMoveSpecimenStart1().position, RedSpecimenCoordinates.getStart().heading)
+                .splineToConstantHeading(RedSpecimenCoordinates.getMoveSpecimenEnd1().position, RedSpecimenCoordinates.getStart().heading)
+
+                .splineToConstantHeading(RedSpecimenCoordinates.getMoveSpecimenStart1().position, RedSpecimenCoordinates.getStart().heading)
+
+                .splineToConstantHeading(RedSpecimenCoordinates.getMoveSpecimenStart2().position, RedSpecimenCoordinates.getStart().heading)
+                .splineToConstantHeading(RedSpecimenCoordinates.getMoveSpecimenEnd2().position, RedSpecimenCoordinates.getStart().heading)
+
+                .splineToConstantHeading(RedSpecimenCoordinates.getMoveSpecimenStart2().position, RedSpecimenCoordinates.getStart().heading)
+
+                .splineToConstantHeading(RedSpecimenCoordinates.getMoveSpecimenStart3().position, RedSpecimenCoordinates.getStart().heading)
+                .splineToConstantHeading(RedSpecimenCoordinates.getMoveSpecimenEnd3().position, RedSpecimenCoordinates.getStart().heading).build();
+
+        Action collectSecond = ignitionSystem.actionBuilder(RedSpecimenCoordinates.getMoveSpecimenEnd3())
+                .setTangent(RedSpecimenCoordinates.getStart().heading)
+                .splineToConstantHeading(RedSpecimenCoordinates.getIntake().position, RedSpecimenCoordinates.getStart().heading).build();
+
+        Action scoreSecond = ignitionSystem.actionBuilder(RedSpecimenCoordinates.getIntake())
+                .strafeToConstantHeading(RedSpecimenCoordinates.getScore2().component1()).build();
+
+        Action collectThird = ignitionSystem.actionBuilder(RedSpecimenCoordinates.getScore2())
+                .strafeToConstantHeading(RedSpecimenCoordinates.getIntake().position).build();
+
+        Action scoreThird = ignitionSystem.actionBuilder(RedSpecimenCoordinates.getIntake())
+                .strafeToConstantHeading(RedSpecimenCoordinates.getScore3().component1()).build();
+
+        Action collectFourth = ignitionSystem.actionBuilder(RedSpecimenCoordinates.getScore3())
+                .strafeToConstantHeading(RedSpecimenCoordinates.getIntake().position).build();
+
+        Action scoreFourth = ignitionSystem.actionBuilder(RedSpecimenCoordinates.getIntake())
+                .strafeToConstantHeading(RedSpecimenCoordinates.getScore4().component1()).build();
+
+        Action collectFifth = ignitionSystem.actionBuilder(RedSpecimenCoordinates.getScore3())
+                .strafeToConstantHeading(RedSpecimenCoordinates.getIntake().position).build();
+
+        Action scoreFifth = ignitionSystem.actionBuilder(RedSpecimenCoordinates.getIntake())
+                .strafeToConstantHeading(RedSpecimenCoordinates.getScore5().component1()).build();
+
+        Action park = ignitionSystem.actionBuilder(RedSpecimenCoordinates.getScore5())
+                .strafeToConstantHeading(RedSpecimenCoordinates.getPark().position).build();
 
         waitForStart();
 
@@ -38,12 +80,13 @@ public class RedSpecimen5Park extends LinearOpMode {
                 new SequentialAction(
                         scorePreLoad,
                         moveSpecimens,
+                        collectSecond,
                         scoreSecond,
-                        intakeThird,
+                        collectThird,
                         scoreThird,
-                        intakeFourth,
+                        collectFourth,
                         scoreFourth,
-                        intakeFifth,
+                        collectFifth,
                         scoreFifth,
                         park
                 )
