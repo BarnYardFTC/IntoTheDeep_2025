327f72533acbf312437fbc089d3b2e8d42f21187
BarnYard <barnyard22993@gmail.com>
2025-02-27 05:24:57 +0200

    limelight pipeline(not tested)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/LimeLight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/LimeLight.java
@@ -14,11 +14,23 @@ import java.util.List;
 
 @TeleOp(name = "Limelight3A")
 public class LimeLight extends LinearOpMode{
+    int corner1X;
+    int corner1Y;
+    int corner2X;
+    int corner2Y;
+    int corner3X;
+    int corner3Y;
+    int corner4X;
+    int corner4Y;
+
+    int angle;
+
     private Limelight3A limelight;
 
     @Override
     public void runOpMode() throws InterruptedException {
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        Differential.initialize(this);
 
         telemetry.setMsTransmissionInterval(11);
 
@@ -33,38 +45,44 @@ public class LimeLight extends LinearOpMode{
 
         while (opModeIsActive()) {
             LLResult result = limelight.getLatestResult();
-
-            LLStatus status = limelight.getStatus();
-            telemetry.addData("Pipeline", "Index: %d, Type: %s",
-                    status.getPipelineIndex(), status.getPipelineType());
             if (result != null) {
                 if (result.isValid()) {
                     // Access color results
                     List<LLResultTypes.ColorResult> colorResults = result.getColorResults();
+
                     for (LLResultTypes.ColorResult cr : colorResults) {
-                        int corner1X;
-                        int corner1Y;
-                        int corner2X;
-                        int corner2Y;
-
-                        double angle;
-
                         if (cr.getTargetCorners().size() == 4) {
-                            corner1X = (int) (cr.getTargetCorners().get(2).get(0) - cr.getTargetCorners().get(2).get(0) % 1);
-                            corner1Y = (int) (cr.getTargetCorners().get(2).get(1) - cr.getTargetCorners().get(2).get(1) % 1);
+                            corner1X = (int) (cr.getTargetCorners().get(0).get(0) - cr.getTargetCorners().get(0).get(0) % 1);
+                            corner1Y = (int) (cr.getTargetCorners().get(0).get(1) - cr.getTargetCorners().get(0).get(1) % 1);
                             corner2X = (int) (cr.getTargetCorners().get(1).get(0) - cr.getTargetCorners().get(1).get(0) % 1);
                             corner2Y = (int) (cr.getTargetCorners().get(1).get(1) - cr.getTargetCorners().get(1).get(1) % 1);
+                            corner3X = (int) (cr.getTargetCorners().get(2).get(0) - cr.getTargetCorners().get(2).get(0) % 1);
+                            corner3Y = (int) (cr.getTargetCorners().get(2).get(1) - cr.getTargetCorners().get(2).get(1) % 1);
+                            corner4X = (int) (cr.getTargetCorners().get(3).get(0) - cr.getTargetCorners().get(3).get(0) % 1);
+                            corner4Y = (int) (cr.getTargetCorners().get(3).get(1) - cr.getTargetCorners().get(3).get(1) % 1);
 
-                            if (corner1X == corner2X) {
+                            if (corner1X <= corner4X - 10 && corner1X >= corner4X + 10) {
                                 angle = 0;
                             }
-                            else if (corner1Y == corner2Y) {
+                            else if (corner1Y <= corner4Y - 10 && corner1Y >= corner4Y + 10) {
                                 angle = 90;
                             }
                             else {
-                                angle = Math.toDegrees(Math.atan((double) (corner1Y - corner2Y) / (corner1X - corner2X)));
-                                if (angle < 0) {
-                                    angle = 180 + angle;
+                                int newAngle = ((int) Math.toDegrees(Math.atan((double) (corner1Y - corner4Y) / (corner1X - corner4X))));
+                                if (newAngle >= angle + 5 || newAngle <= angle - 5) {
+                                    angle = newAngle;
+                                    if (angle < 0) {
+                                        angle = 180 + angle;
+                                    }
+                                }
+                            }
+
+                            if (gamepad1.y) {
+                                if (angle - 90 < 0) {
+                                    Differential.move(angle + 90, 0);
+                                }
+                                else {
+                                    Differential.move(angle - 90, 0);
                                 }
                             }
                         }
@@ -73,8 +91,6 @@ public class LimeLight extends LinearOpMode{
                         }
 
                         telemetry.addData("angle", angle);
-                        telemetry.addData("size", cr.getTargetCorners().size());
-                        telemetry.addData("corners", cr.getTargetCorners());
                     }
                 }
             } else {
